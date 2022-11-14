d94122d30bcecc0425116b339f7a044638a7f000
thurain <thurainbowin.dev@gmail.com>
2022-11-14 19:28:11 +0630

    - write book detail tests

--- app/src/androidTest/java/com/thurainx/libraryapp/uitests/userflow1/TestBookDetailUI.kt
+++ app/src/androidTest/java/com/thurainx/libraryapp/uitests/userflow1/TestBookDetailUI.kt
@@ -3,8 +3,10 @@ package com.thurainx.libraryapp.uitests.userflow1
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.levibostian.recyclerviewmatcher.RecyclerViewMatcher
@@ -31,7 +33,7 @@ class TestBookDetailUI {
 
     @Test
     fun onTapBook_checkBookDetailUI(){
-        Espresso.onView(ViewMatchers.withText("Hardcover Nonfiction"))
+        Espresso.onView(withText("Hardcover Nonfiction"))
             .perform(NestedScrollViewExtension())
 
         Espresso.onView(
@@ -44,11 +46,120 @@ class TestBookDetailUI {
 
         Thread.sleep(1000L)
 
-        Espresso.onView(ViewMatchers.withId(R.id.btnMovieDetailBack))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // app bar
+        displayAppBar()
+
+        // book detail
+        displayBookDetail()
+
+        // rating & reviews
+        displayRatingAndReviews()
+
+        // author & policy
+        displayAuthorAndPolicy()
+
+    }
+
+    private fun displayAppBar(){
+        Espresso.onView(withId(R.id.btnMovieDetailBack))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.ivBookDetailSearch))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.ivBookDetailAddToLibrary))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.ivBookDetailMore))
+            .check(matches(isDisplayed()))
+    }
+
+    private fun displayBookDetail(){
+        Espresso.onView(withId(R.id.tvBookDetailTitle))
+            .check(matches(withText("THE BOYS FROM BILOXI")))
+
+        Espresso.onView(withId(R.id.tvBookDetailAuthor))
+            .check(matches(withText("John Grisham")))
+
+        Espresso.onView(withId(R.id.tvBookDetailReleasedDate))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.llReviewAndRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.llBookType))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.llPageCount))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutBookDetailAboutBook))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.tvBookDetailDescription))
+            .check(matches(withText("Two childhood friends follow in their fathers’ footsteps, which puts them on opposite sides of the law.")))
+
+    }
+
+    private fun displayRatingAndReviews(){
+
+        Espresso.onView(withId(R.id.layoutBookDetailRating))
+            .perform(NestedScrollViewExtension())
+
+        Espresso.onView(withId(R.id.layoutBookDetailRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.tvBookDetailRatingLarge))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.rbBookDetailRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.tvBookDetailReviewCountLarge))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutFiveStarRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutFourStarRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutThreeStarRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutTwoStarRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutOneStarRating))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.rvBookDetailComments))
+            .check(matches(isDisplayed()))
+
+    }
+
+    private fun displayAuthorAndPolicy(){
+        Espresso.onView(withId(R.id.layoutBookDetailVAT))
+            .perform(NestedScrollViewExtension())
+
+        Thread.sleep(1000L)
+
+        Espresso.onView(withId(R.id.layoutBookDetailAboutAuthor))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.tvBookDetailAboutAuthorDescription))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutBookDetailRefund))
+            .check(matches(isDisplayed()))
+
+        Espresso.onView(withId(R.id.layoutBookDetailVAT))
+            .check(matches(isDisplayed()))
 
 
     }
+
+
 
 
 
